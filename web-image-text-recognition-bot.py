afa7afd61277731cf5fa26a1410d4a0fa99470b0
minaton-ru <minaton@yandex.ru>
2023-04-06 21:46:48 +0300

    aiohttp вместо requests, используем функции

--- web-image-text-recognition-bot.py
+++ web-image-text-recognition-bot.py
@@ -1,6 +1,7 @@
-import requests
+import io
 import configparser
 import asyncio
+import aiohttp
 import pytesseract
 from PIL import Image
 from aiogram import Bot, Dispatcher, types
@@ -12,21 +13,29 @@ config.read("config.ini")
 TOKEN = config['Telegram']['API_TOKEN']
 
 URL = 'http://новоспасский-монастырь.рф/raspisanie' # Адрес страницы, с которой будем забирать картинку
-contents = requests.get(URL).text # Забираем содержимое этой страницы
-soup = BeautifulSoup(contents, 'html.parser') # Смотрим содержимое через HTML-парсер
-my_img = soup.find("div", class_="leading-0").p.img.get('src') # Берем div, у которого нужный нам class, внутри этого div есть параграф, внутри которого нужный нам файл картинки, из которого мы забираем содержимое атрибута src
-img_url = "http://xn----7sbbf5agftchdbghyfcaiu3qxa.xn--p1ai" + my_img # Составляем адрес для картинки
+tesseract_config = r'--psm 6' # Настройка для распознавания текста с картинки
 
-tesseract_config = r'--psm 6' # Настройка для распознавания текста с картинки 
+async def get_img_url(URL):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as response:  # Забираем содержимое этой страницы
+            html = await response.text() # Смотрим содержимое через HTML-парсер
+            soup = BeautifulSoup(html, 'html.parser') # Смотрим содержимое через HTML-парсер
+            my_img = soup.find("div", class_="leading-0").p.img.get('src') # Берем div, у которого нужный нам class, внутри этого div есть параграф, внутри которого нужный нам файл картинки, из которого мы забираем содержимое атрибута src
+            img_url = "http://xn----7sbbf5agftchdbghyfcaiu3qxa.xn--p1ai" + my_img # Составляем адрес для картинки
+            return img_url
 
-# Загрузка изображения с текстом
-with Image.open(requests.get(img_url, stream=True).raw) as image:
-    string = pytesseract.image_to_string(image, lang='rus', config=tesseract_config) # Кладем сюда распознанный текст
+async def text_recog_from_img(img_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(img_url) as response: # Загрузка нужной картинки, на которой текст
+            data = await response.read()
+            file = Image.open(io.BytesIO(data))
+            string = pytesseract.image_to_string(file, lang='rus', config=tesseract_config) # Кладем сюда распознанный текст
+            return string
 
 bot = Bot(token=TOKEN) # Инициализация бота
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands="start")
+@dp.message_handler(commands="start") # Бот по команде start показывает приветственный текст и две кнопки
 async def command_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Текст", "Картинка"]
@@ -35,10 +44,13 @@ async def command_start(message: types.Message):
 
 @dp.message_handler(lambda message: message.text == "Текст")
 async def send_text(message: types.Message):
+    img_url = await get_img_url(URL)
+    string = await text_recog_from_img(img_url)
     await message.answer(string) # Высылает распознанный текст
 
 @dp.message_handler(lambda message: message.text == "Картинка")
 async def send_text(message: types.Message):
+    img_url = await get_img_url(URL)
     await bot.send_photo(message.chat.id, types.InputFile.from_url(img_url)) # Высылает картинку
 
 async def main():
